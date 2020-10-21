944445ff423ec13eb99911f5ddf49d0b56da2d95
Fredrik <fredrikf2000@gmail.com>
2020-10-21 13:31:04 +0200

    added images

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// selecting the slider
+let slider = document.getElementById("slider");
+
+// adding event listener
+slider.addEventListener("input", sliderChange);
+
+function sliderChange() {
+
+}
